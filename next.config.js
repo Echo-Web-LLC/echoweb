a59096e1a4a8247af4ecb082280dd1812db4d260
Andrew Kal <andrewkalgit@gmail.com>
2023-06-29 22:29:58 -0400

    deploy settings

--- next.config.js
+++ next.config.js
@@ -2,17 +2,3 @@
 const nextConfig = {}
 
 module.export = nextConfig
-
-module.exports = {
-    reactStrictMode: true,
-    images: {
-    remotePatterns: [
-        {
-        protocol: 'https',
-        hostname: 'echowebllc-dev.s3.amazonaws.com',
-        port: '',
-        pathname: '/public/**',
-        },
-    ],
-    },
-}
